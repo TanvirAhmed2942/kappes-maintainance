e861bf2f1e16910a7e4cea6a40d399d77e8d4e88
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-12-10 16:35:47 +0600

    all ok

--- src/components/maintainance/Maintainance.jsx
+++ src/components/maintainance/Maintainance.jsx
@@ -5,17 +5,12 @@ function Maintainance() {
   return (
     <div className="w-full h-full relative">
       <Marquee className="text-2xl font-bold text-center py-4 bg-kappes ">
-        <span className="text-white mx-4">
-          This Website is Under Maintenance
-        </span>
         <span className="text-white mx-4">Content Coming Soon</span>
-        <span className="text-white mx-4">
-          This Website is Under Maintenance
-        </span>
         <span className="text-white mx-4">Content Coming Soon</span>
-        <span className="text-white mx-4">
-          This Website is Under Maintenance
-        </span>
+        <span className="text-white mx-4">Content Coming Soon</span>
+        <span className="text-white mx-4">Content Coming Soon</span>
+        <span className="text-white mx-4">Content Coming Soon</span>
+        <span className="text-white mx-4">Content Coming Soon</span>
         <span className="text-white mx-4">Content Coming Soon</span>
       </Marquee>
       <Image
